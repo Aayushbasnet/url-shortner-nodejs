aa0408e39067be5972f9e6f6d88c1033e10fb2d1
Aayush Basnet <aayushbasnet07@gmail.com>
2022-05-03 15:42:34 +0545

    session added

--- src/server/controller/server.ts
+++ src/server/controller/server.ts
@@ -4,32 +4,88 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysqlConnection = require('../modules/mySqlConnection');
+const session = require('express-session');
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+}))
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true})); // bodyParser can be replaced with express as well
 app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
+    // session value
+    const shortUrl = req.session.shortUrl;
+    const originalUrl = req.session.originalUrl;
+    delete req.session.shortUrl;
+    delete req.session.originalUrl;
+    
+    // database connection
     mysqlConnection.getConnection((error, conn) =>{
-    const dbquery: string = "SELECT * FROM urlshortners";
-        conn.query(dbquery, (error, rows, fields) => {
-            if(!!error){
-                console.log("Failed to retrieve data", error);
-                res.render("index", {data:error});
-            }else{
-                console.log("retrived successfully \n");
-                // console.log(rows);
-                res.render("index", {data:rows});
+        // database queries
+        if(!!error){
+            console.log("Cannot get connection");
+            res.status(500).json({
+                status: "No connection",
+                message: "Failed to connect database"
+            });
+        }else{
+            const dbquery: string = "SELECT * FROM urlshortners";
+            conn.query(dbquery, (error, rows, fields) => {
+                if(!!error){
+                    console.log("Failed to retrieve data", error);
+                    res.render("index", {data:error});
+                }else{
+                    console.log("retrived successfully \n");
+                    res.render("index", {data:{rows, shortUrl, originalUrl}});  // rows:rows are same show only rows
+                    // parse rows/fields
+                };
+            });
+        }
 
-                // parse rows/fields
-            }
-        });
     });
     
 });
 
 const shortenRoute = require('../routes/shorten');
 app.use('/shorten', shortenRoute);
+
+app.get('/:urlKey', (req, res) => {
+    const key = req.params.urlKey;
+    console.log(key);
+    mysqlConnection.getConnection((error, conn) => {
+        if(!!error){
+            console.log("Cannot get Connection", error);
+            res.status(500).json({
+                status: "No connection",
+                message: "Failed to connect database"
+            });
+        }else{
+            const dbquery : string = 
+                `SELECT OriginalUrl FROM urlshortners WHERE UrlKey = "${key}"`;
+            conn.query(dbquery, (error, rows, fields) => {
+                if(!!error){
+                    console.log("Query error", error);
+                    res.status(500).json({
+                        status: "Bad Query",
+                        message: "Cannot retrieve original url"
+                    });
+                }else{
+                    console.log("Original Url: ", rows);
+                    if(rows[0] == null){
+                        res.redirect('/');
+                    } else{
+                        res.redirect(`${rows[0].OriginalUrl}`); 
+
+                    }                   
+                };
+            });
+        }
+    });
+});
 
 // listen
 app.listen(5000,() => {
--- src/server/routes/shorten.ts
+++ src/server/routes/shorten.ts
@@ -1,21 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const mysqlConnection = require('../modules/mySqlConnection');
+const session = require('express-session');
+router.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+}))
 
 router.get('/', (req,res) => {
     res.send(`Hi I am going to shorten ${req.body.userUrl}`);
 });
 
 router.post('/', (req,res) => {
-    const originalUrl = req.body.userUrl;
+    const originalUrl = req.body.userUrl;   //original url from form
     // console.log(originalUrl);
     if(originalUrl.length !== 0){
         try {
+            // generating random short url key
+            const uniqueId = Math.random().toString(36).replace(/[^a-z0-9]/gi,'').substring(2,10);
+            // storing uniqueid in session
+            req.session.shortUrl = "http://localhost:5000/"+uniqueId;    
+            req.session.originalUrl = originalUrl;
+
             // inserting value in database
             mysqlConnection.getConnection((error, conn) => {
-                const uniqueId = Math.random().toString(36).replace(/[^a-z0-9]/gi,'').substring(2,10);
-                console.log(uniqueId);
-                const dbquery:string = `INSERT INTO urlshortners (OriginalUrl, ShortenUrl) VALUES ("${originalUrl}", "${uniqueId}")`;
+                const dbquery:string = `INSERT INTO urlshortners (OriginalUrl, ShortenUrl, UrlKey) VALUES ("${originalUrl}", "http://localhost:5000/${uniqueId}","${uniqueId}")`;
                 conn.query(dbquery,(error, rows,) =>{
                     if(!!error){
                         res.sendStatus(500).json({
